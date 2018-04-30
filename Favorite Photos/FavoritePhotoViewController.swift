dee71a00ca43cb7828bcd0c0ef31e5bef140587d
Yizhi Feng <fengy2@rose-hulman.edu>
2018-04-30 11:58:08 -0400

    user interface setup

---
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FavoritePhotoViewController.swift
+//  Favorite Photos
+//
+//  Created by FengYizhi on 2018/4/30.
+//  Copyright © 2018年 FengYizhi. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePhotoViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var progressView: UIProgressView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pressedFab(_ sender: Any) {
+        
+    }
+    
+}
---
+++ Favorite Photos/PhotoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoListViewController.swift
+//  Favorite Photos
+//
+//  Created by FengYizhi on 2018/4/30.
+//  Copyright © 2018年 FengYizhi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
