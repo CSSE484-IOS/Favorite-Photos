9360d955f3620ebc08b2d85c84a3ea6cf7ff946f
Yizhi Feng <fengy2@rose-hulman.edu>
2018-04-30 12:31:24 -0400

    saving a photo to firebase storage

--- Favorite Photos/FavoritePhotoViewController.swift
+++ Favorite Photos/FavoritePhotoViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 class FavoritePhotoViewController: UIViewController{
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressView: UIProgressView!
+    
+    var photoStorageRef: StorageReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        photoStorageRef = Storage.storage().reference(withPath: "favorite")
     }
 
     @IBAction func pressedFab(_ sender: Any) {
@@ -27,6 +31,28 @@ class FavoritePhotoViewController: UIViewController{
         
         present(imagePicker, animated: true)
     }
+    
+    func uploadImage(_ data: Data?) {
+        guard let data = data else { return }
+        let uploadMetadata = StorageMetadata()
+        uploadMetadata.contentType = "image/jpeg"
+        progressView.isHidden = false
+        progressView.progress = 0
+        
+        let uploadTask = photoStorageRef.putData(data, metadata: uploadMetadata) { (metadata, error) in
+            if let error = error {
+                print("Error with upload \(error.localizedDescription)")
+            }
+        }
+        uploadTask.observe(.progress) { (snapshot) in
+            guard let progress = snapshot.progress else { return }
+            self.progressView.progress = Float(progress.fractionCompleted)
+        }
+        uploadTask.observe(.success) { (snapshot) in
+            self.progressView.isHidden = true
+            print("Your upload is finished")
+        }
+    }
 }
 
 // MARK: UIImagePicker controller delegate methods
@@ -38,7 +64,8 @@ extension FavoritePhotoViewController: UINavigationControllerDelegate, UIImagePi
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            self.imageView.image = image
+//            self.imageView.image = image
+            uploadImage(UIImageJPEGRepresentation(image, 0.5))
         }
         picker.dismiss(animated: true)
     }
